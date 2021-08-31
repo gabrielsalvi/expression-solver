38af77eb441d36dcefa5b4ae7e24f9e937cd9098
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-08-31 01:35:58 -0300

    ula.v is working fine

--- src/components/ula.v
+++ src/components/ula.v
@@ -1,15 +1,10 @@
-module ula (result, h, input1, input2);
+module ula (
+    input h,
+    input [15:0] a,
+    input [15:0] b,
+    output [15:0] result
+);
 
-    input h;
-    input [15:0] input1;
-    input [15:0] input2;
-    output [15:0] result;
+    assign result = h ? a * b : a + b;
 
-    always @(h or input1 or input2) 
-    begin
-        if (h == 1'b0) 
-            result <= input1 + input2;
-        else
-            result <= input1 * input2; 
-    end
 endmodule
