d271fc661224adca20458a276a87bbb3944d45d1
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-09-01 18:48:33 -0300

    creating the blocks: operative.v and control.v

---
+++ src/blocks/control.v
@@ -0,0 +1,96 @@
+module control (
+    input clk,
+    input rst,
+    input start,
+    output LX,
+    output LS,
+    output LH,
+    output H,
+    output [1:0] M0,
+    output [1:0] M1,
+    output [1:0] M2,
+    output completed
+);
+
+    parameter A = 3'b000, B = 3'b001, C = 3'b010, D = 3'b011, E = 3'b100, F = 3'b101, G = 3'b110;
+    reg [3:0] state = A;
+    
+    always @(posedge clk or rst) 
+    begin
+        if (rst) state <= A;
+        else begin
+            case (state)
+                A : begin
+                    LX <= 1'b0;
+                    M0 <= 2'b00;
+                    M1 <= 2'b00;
+                    M2 <= 2'b00;
+                    H <= 1'b0;
+                    LS <= 1'b0;
+                    LH <= 1'b0;
+                    completed <= 1'b0;
+
+                    if (start == 1'b1) state <= B;
+                end
+
+                B : begin
+                    LX <= 1'b1;
+                    M1 <= 2'b01;
+                    H <= 1'b1;
+
+                    state <= C;
+                end
+
+                C : begin
+                    LX <= 1'b0;
+                    LS <= 1'b1;
+
+                    state <= D;
+                end
+
+                D : begin
+                    M0 <= 2'b01;
+                    M1 <= 2'b00;
+                    M2 <= 2'b10;
+                    LS <= 1'b0;
+                    LH <= 1'b1;
+
+                    state <= E;
+                end
+
+                E : begin
+                    M0 <= 2'b10;
+                    M2 <= 2'b00;
+                    LS <= 1'b1;
+                    LH <= 1'b0;
+
+                    state <= F;
+                end
+                
+                F : begin
+                    M1 <= 2'b11;
+                    M2 <= 2'b10;
+                    H <= 1'b0;
+                    LS <= 1'b0;
+                    LH <= 1'b1;
+
+                    state <= G;
+                end
+
+                G : begin
+                    M0 <= 2'b11;
+                    M1 <= 2'b00;
+                    LS <= 1'b1;
+                    LH <= 1'b0;
+
+                    state <= H;
+                end
+
+                H : begin
+                    LS <= 1'b0;
+                    completed <= 1'b1;
+                end
+            endcase
+        end
+    end
+endmodule
--- src/blocks/operative.v
+++ src/blocks/operative.v
@@ -1,7 +1,42 @@
-module operativo (variaveis ...);
+`include "../components/alu.v"
+`include "../components/mux.v"
+`include "../components/register.v"
 
-    `include "components/*"
+module operative 
+(
+    input clk,
+    input [7:0] input_X,
+    input [15:0] A,
+    input [15:0] B,
+    input [15:0] C,
+    input LX,
+    input LS,
+    input LH,
+    input H,
+    input [1:0] M0,
+    input [1:0] M1,
+    input [1:0] M2,
+    output zero,
+    output overflow,
+    output [15:0] result
+);
 
+    parameter z = 16'b0000000000000000;
+    wire [15:0] X, Reg_X, Reg_S, Reg_H, M0_out, M1_out, M2_out, ula_out;
 
-    
+    assign X = input_X + z;
+
+    mux mux_0 (M0, z, A, B, C, M0_out);
+    mux mux_1 (M1, M0_out, Reg_X, Reg_S, Reg_H, M1_out);
+    mux mux_2 (M2, Reg_S, M0_out, Reg_S, Reg_H, M2_out);
+
+    register reg_x (clk, rst, LX, X, Reg_X);
+    register reg_s (clk, rst, LS, ula_out, Reg_S);
+    register reg_h (clk, rst, LH, ula_out, Reg_H);
+
+    alu alu_0 (H, A, B, ula_out);
+
+    assign result = Reg_S;
+    assign zero = result ? 1'b0 : 1'b1;
+
 endmodule
